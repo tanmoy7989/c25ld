bc4b93747ac45f8591653e226ced0fbcd20fe4d6
tanmoy7989 <tanmoy.7989@gmail.com>
2015-08-18 14:12:55 -0700

    corrected dir creation

--- makeLammps/setup_transferability_runs.py
+++ makeLammps/setup_transferability_runs.py
@@ -4,25 +4,23 @@ import numpy as np
 import os, sys
 
 # Globals
-curr_dir = sys.argv[1]
+trans_dir = sys.argv[1]
+curr_month = 'aug15'
 c_lens = [15]
 
 # Dir hierarchy
 def build_dir(c_len):
-	parent_dir = 'aug15_runs_c%d' % c_len
+	parent_dir = '%s_runs_c%d' % (curr_month, c_len)
 	print 'Building %s ...' % parent_dir
-	return os.path.join(curr_dir, parent_dir)
+	os.mkdir(os.path.join(trans_dir, parent_dir))
+	return os.path.join(trans_dir, parent_dir)
 	
 # Generate Lammps script for specific chain length
 def genLammpsJobs(parent_dir, c_len, n_water= 1700, n_poly = 1):
 	print 'Loading stuff into %s ...' % parent_dir
-	makeLammpsHome = os.path.expanduser('~/c25ld/makeLammps')
-	os.chdir(makeLammpsHome)
 	cmdstring = 'python writelammpsjob.py %s %d %d %d 0' % (parent_dir, c_len, n_water, n_poly)
 	os.system(cmdstring)
-	os.chdir(curr_dir)
 	
-
 
 # Main
 for c_len in c_lens:
