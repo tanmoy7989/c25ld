3bf98b9d32d3d6a5b9e60e768480f0116ef6113c
tanmoy7989 <tanmoy.7989@gmail.com>
2015-08-26 16:32:35 -0700

    getBoxLen is a pain

--- analysis/getBoxLen.py
+++ analysis/getBoxLen.py
@@ -21,10 +21,9 @@ def getInitBoxLen(datafile):
 	
 
 def getNPTBoxLen(restartfile, ff_file):
-	#if not os.path.isfile(restartfile):
-	#	raise IOError('%s not present' % restartfile.split('/')[-1])
-	
-	
+	if not os.path.isfile(restartfile):
+		raise IOError('%s not present' % restartfile.split('/')[-1])
+		
 	os.chdir(os.path.dirname(restartfile))
 	new_datafile = os.path.join(os.path.dirname(restartfile), 'tmp_res2data.dat')
 	cmdstring = 'restart2data.py %s %s %s' % (restartfile, new_datafile, ff_file)
@@ -37,8 +36,3 @@ def getNPTBoxLen(restartfile, ff_file):
 	os.remove(new_datafile)
 	return boxlen
 	
-	
-##test
-restartfile = sys.argv[1]
-ff_file = sys.argv[2]
-print getNPTBoxLen(restartfile, ff_file)
